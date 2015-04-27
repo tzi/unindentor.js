fed38fc87f5b4893d136aaa4c83c5f413d1b24da
Thomas ZILLIOX <thomas@zilliox.me>
2015-04-28 01:08:05 +0200

    First commit: small step & great expectations

---
+++ README.md
@@ -0,0 +1,8 @@
+unindentor.js
+===============
+
+A small library to remove indentation from `textarea` and `contenteditable` elements.
+
+```js
+```
+
---
+++ unindentor.js
@@ -0,0 +1,67 @@
+var unindentor = (function () {
+    'use strict';
+
+    function unindentElementList(elementList) {
+        elementList = formatElementList(elementList);
+        for (var i = 0; i < elementList.length; i++) {
+            unindentElement(elementList[i]);
+        }
+    }
+
+    function formatElementList(elementList) {
+        if (typeof elementList == 'string') {
+            elementList = document.querySelectorAll(elementList);
+        }
+        elementList = Array.prototype.slice.call(elementList);
+        return elementList;
+    }
+
+    function unindentElement(element) {
+        element.innerHTML = unindentText(element.innerHTML);
+    }
+
+    function unindentText(text) {
+        var result = [];
+        var indentation = false;
+        var lines = text.split('\n');
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            if (indentation === false) {
+                var indentation = findIndentation(line);
+                if (!indentation) {
+                    continue;
+                }
+            }
+            line = unindentLine(line, indentation);
+            result.push(line);
+        }
+        return result.join('\n');
+    }
+
+    function unindentLine(line, indentation) {
+        if (line.startsWith(indentation)) {
+            line = line.substr(indentation.length);
+        }
+        return line;
+    }
+
+    function findIndentation(line) {
+        var trimmed = line.trim();
+        if (!trimmed) {
+            return false;
+        }
+        return line.substr(0, line.indexOf(trimmed[0]));
+    }
+
+    return {
+        unindentElementList: unindentElementList,
+        unindentElement: unindentElement,
+        unindentText: unindentText
+    }
+})();
+
+if (typeof String.prototype.startsWith != 'function') {
+    String.prototype.startsWith = function (str) {
+        return this.slice(0, str.length) == str;
+    };
+}
